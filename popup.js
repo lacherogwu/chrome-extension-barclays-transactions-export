56a9c571254a351af94bbeb8499e3386b62984be
lachero <asaf@utopia.tech>
2022-06-29 16:30:13 +0300

    fixed getCurrentTab

--- popup.js
+++ popup.js
@@ -46,6 +46,49 @@ function setFeedbackState(html = '', className = '') {
 	feedback.innerHTML = html;
 }
 
+function saveAs(blob, filename) {
+	const url = window.URL.createObjectURL(blob);
+	const _element = document.createElement('a');
+	_element.href = url;
+	_element.setAttribute('download', filename);
+	_element.click();
+}
+
+function generateCsv(items, filename) {
+	const csv = Papa.unparse(items);
+	const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+	saveAs(blob, filename);
+}
+
+async function getCurrentTab() {
+	let queryOptions = { active: true, currentWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+
+	return tab;
+}
+
+function getStatementDateElementValue() {
+	return document.querySelector('#tp_cycles')?.value;
+}
+
+async function getStatementDate() {
+	const tab = await getCurrentTab();
+	const data = await chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		func: getStatementDateElementValue,
+	});
+
+	return data[0]?.result;
+}
+
+function getDateString(date) {
+	if (!date) return getDateString(new Date().toISOString());
+	return date.split('T')[0];
+}
+
+const isBarclaysWebsite = string => /^https:\/\/\w+\.barclaycardus.com/.test(string);
+
+// handlers
 async function handleClick(event) {
 	event.preventDefault();
 	setFeedbackState();
@@ -82,51 +125,10 @@ async function handleClick(event) {
 	setButtonState();
 }
 
-download.addEventListener('click', handleClick);
-
-function saveAs(blob, filename) {
-	const url = window.URL.createObjectURL(blob);
-	const _element = document.createElement('a');
-	_element.href = url;
-	_element.setAttribute('download', filename);
-	_element.click();
-}
-
-function generateCsv(items, filename) {
-	const csv = Papa.unparse(items);
-	const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-	saveAs(blob, filename);
-}
-
-async function getCurrentTab() {
-	let queryOptions = { active: true };
-	let [tab] = await chrome.tabs.query(queryOptions);
-
-	return tab;
-}
-
-function getStatementDateElementValue() {
-	return document.querySelector('#tp_cycles')?.value;
-}
-
-async function getStatementDate() {
+async function onLoadHandler() {
+	console.log('Loaded');
 	const tab = await getCurrentTab();
-	const data = await chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		func: getStatementDateElementValue,
-	});
-
-	return data[0]?.result;
-}
-
-function getDateString(date) {
-	if (!date) return getDateString(new Date().toISOString());
-	return date.split('T')[0];
-}
-
-const isBarclaysWebsite = string => /^https:\/\/\w+\.barclaycardus.com/.test(string);
-window.onload = async function () {
-	const tab = await getCurrentTab();
+	console.log(tab);
 	if (!tab || !isBarclaysWebsite(tab.url)) {
 		const html = 'You must open <a class="underline" target="_blank" href="https://www.barclaycardus.com">https://www.barclaycardus.com</a>';
 		setFeedbackState(html, 'error');
@@ -134,4 +136,8 @@ window.onload = async function () {
 	}
 
 	download.removeAttribute('disabled');
-};
+}
+
+// events
+download.addEventListener('click', handleClick);
+window.onload = onLoadHandler;
